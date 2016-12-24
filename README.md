2c17c35b2cbcec6494244583683b8fe4014746e6
Markandeya <vasalpha@gmail.com>
2016-12-24 11:24:08 +0530

    Makes Bootstrap(class) code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Shout
-My first php application based on mvc created from scratch.
-A simple app where users can login and post messages on the board.
-#learnbydoing
+My first php application based on MVC, created from scratch.
+This is a simple web app where users can register themselves, login and post 'shouts' on the board.
+~learnbydoing
 :)
--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -1,5 +1,43 @@
 <?php
   class Bootstrap{
-    
-  }
+    private $controller;
+    private $action;
+    private $request;
+
+    public function __construct($request){
+      //Grab the url controller
+      if($request['controller'] == ''){
+         $this->controller = 'home';
+      }else {
+         $this->controller = $request['controller'];
+      }
+
+      //Grab the url action
+      if($request['action'] == ''){
+        $this->action = 'index';
+      }else {
+        $this->action = $request['action'];
+      }
+      echo 'Bootstrap class created';
+    }
+    public function createController(){
+      //Check newController class exists
+      if(class_exists($this->controller)){
+        $parents = class_parents($this->controller);
+        //Check extends baseController
+        if(in_array('Controller', $parents)){
+          //Check specified method exists in newController
+          if(method_exists($this->controller, $this->action)){
+            return new $this->controller($this->action, $this->request);
+          } else{
+            echo '<h1>Method doesn\'t exist</h1>';
+            return;
+          }
+        }
+      } else{
+        echo '<h1>Base Controller not found</h1>';
+        return;
+      }
+    }
+}
  ?>
